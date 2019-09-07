5a6e4028b4f743b7e0cf63a31a94861a9b2abc41
Ruddy Galo <galope_03@hotmail.com>
2019-09-07 03:06:05 -0600

    idk what im doing

--- js/javascript.js
+++ js/javascript.js
@@ -2,9 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("contenedor").style.top = "0";
+        document.getElementsByClassName("menu").style.top = "0";
     } else {
-        document.getElementById("contenedor").style.top = "-50px";
+        document.getElementsByClassName("menu").style.top = "-50px";
     }
 }
 
@@ -12,9 +12,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("contenedor").style.top = "0";
+        document.getElementsByClassName("menu").style.top = "0";
     } else {
-        document.getElementById("contenedor").style.top = "-50px";
+        document.getElementsByClassName("menu").style.top = "-50px";
     }
     prevScrollpos = currentScrollPos;
 }
